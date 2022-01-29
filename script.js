1459cdc6c7c9ac6018da8f8f308a62edef263226
MisterGamingTV <jojo.schorn@outlook.de>
2022-01-29 10:07:27 +0100

    initialize

---
+++ script.js
@@ -0,0 +1,24 @@
+let config = require('./config.json');
+
+let options = {
+	options: {
+		debug: false
+	},
+	connection: {
+		secure: true,
+		reconnect: true
+	},
+	identity: {
+		username: config.bot.name,
+		password: config.bot.token
+	},
+	channels: [config.stuff.channel]
+};
+
+
+client.on('connected', (address, port) => {
+	console.log(`[CONNECTED] Connected to channel ${config.stuff.channel} on ${address}:${port}`);
+
+});
+
+client.login()
